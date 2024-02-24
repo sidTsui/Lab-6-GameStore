34226ea141903ad43dd2cf3b3fc82b5ba0fb366a
sidneytsui <smt4601@uncw.edu>
2024-02-24 14:25:17 -0500

    edit rand selector - sidney

--- src/RandomGameSelector.java
+++ src/RandomGameSelector.java
@@ -8,19 +8,23 @@
 //source: help from inclass lecture slides, and javatpoint.com
 
 public class RandomGameSelector {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //initializing array of strings
         String[] games = {"Betrayal at House on the Hill", "Crokinole", "Cthulu Dice", "Doom", "Dungeons & Dragons",
                 "Escape: Curse of the Temple", "Everdell", "Fluxx", "Hollow Knight", "Loonacy", "Love Letter",
                 "Mario Kart", "Mysterium", "Night Cage", "Pathfinder", "Play Me", "Poetry for Neanderthals",
                 "Roll for It", "Root", "Stardew Valley", "Sushi Go", "Tetris", "Ticket To Ride", "Warhammer",
                 "World of Warcraft", "Zombie Dice"};
+
         System.out.println("12 Randomly Selected Games: \n"); //heading
 
-        for (int i = 0; i < 12; i++) {//loop to makes sure only 12 are chosen
+        for (int i = 0; i < 12; ) {//loops to make sure only 12 exist
             int random = (int) (Math.random() * games.length);//random element within range 1-26
-            String AddElement = games[random];//adding to array
-            System.out.println(AddElement);//prints each game
+            if (games[random] != "") {//checking if value exists
+                System.out.println(games[random]); //prints game if it is not empty("")
+                games[random] = "";//delete game when slected so it cannot be chosen again
+                i++;
             }
+        }
     }
 }
