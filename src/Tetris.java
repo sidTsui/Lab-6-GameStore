5c4161596a16ae93a3cc777044a3e582b36b8b3c
ejs6637 <160261312+ejs6637@users.noreply.github.com>
2024-03-06 17:11:02 -0500

    Update Tetris.java
    edit

--- src/Tetris.java
+++ src/Tetris.java
@@ -1,2 +1,20 @@
-//public class Tetris extends VideoGames{
-//}
+public class Tetris extends VideoGames{
+    private String characterName;
+
+    public Tetris(String gameName, String gameType, String creator,
+                     Double currentPrice, int maxPlayerNum, Double playingTime,
+                     String difficulty, Double starReviews) {
+
+        super(gameName, gameType, creator, currentPrice, maxPlayerNum,
+                playingTime, difficulty, starReviews);
+    }
+
+
+
+    public String getCharacterName(){
+        return this.characterName = characterName;
+    }
+    public void setCharacterName(String characterName){
+        this.characterName = characterName;
+    }
+}
