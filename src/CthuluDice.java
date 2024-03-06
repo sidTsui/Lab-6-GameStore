7fc68823015eba8b671836adf34aad56c5550a39
ejs6637 <160261312+ejs6637@users.noreply.github.com>
2024-03-06 16:38:09 -0500

    Update CthuluDice.java
    edit

--- src/CthuluDice.java
+++ src/CthuluDice.java
@@ -1,2 +1,20 @@
-//public class CthuluDice extends BoardGames{
-//}
+public class CthuluDice extends BoardGames{
+    private String characterName;
+
+    public CthuluDice(String gameName, String gameType, String creator,
+                     Double currentPrice, int maxPlayerNum, Double playingTime,
+                     String difficulty, Double starReviews) {
+
+        super(gameName, gameType, creator, currentPrice, maxPlayerNum,
+                playingTime, difficulty, starReviews);
+    }
+
+
+
+    public String getCharacterName(){
+        return this.characterName = characterName;
+    }
+    public void setCharacterName(String characterName){
+        this.characterName = characterName;
+    }
+}
