c0b1f89593b73b931413cf35129f9e4ac4549136
ejs6637 <160261312+ejs6637@users.noreply.github.com>
2024-03-06 17:01:07 -0500

    Update Fluxx.java
    edit (changed fluxx into a card game)

--- src/Fluxx.java
+++ src/Fluxx.java
@@ -1,2 +1,17 @@
-//public class Fluxx extends BoardGames{
-//}
+public class Fluxx extends CardGames{
+    private String cardDesigner;
+
+    public Fluxx(String gameName, String gameType, String creator,
+                     Double currentPrice, int maxPlayerNum, Double playingTime,
+                     String difficulty, Double starReviews) {
+
+        super(gameName, gameType, creator, currentPrice, maxPlayerNum,
+                playingTime, difficulty, starReviews);
+    }
+    public String getCardDesigner(){
+        return this.cardDesigner = cardDesigner;
+    }
+    public void setCardDesigner(String cardDesigner){
+        this.cardDesigner = cardDesigner;
+    }
+}
