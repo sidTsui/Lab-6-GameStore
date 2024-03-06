fb07c4ec9947467aed79261bb8c214137edb9250
ejs6637 <160261312+ejs6637@users.noreply.github.com>
2024-03-06 17:06:11 -0500

    Update LoveLetter.java
    edit

--- src/LoveLetter.java
+++ src/LoveLetter.java
@@ -1,2 +1,17 @@
-//public class LoveLetter extends CardGames{
-//}
+public class LoveLetter extends CardGames{
+    private String cardDesigner;
+
+    public LoveLetter(String gameName, String gameType, String creator,
+                     Double currentPrice, int maxPlayerNum, Double playingTime,
+                     String difficulty, Double starReviews) {
+
+        super(gameName, gameType, creator, currentPrice, maxPlayerNum,
+                playingTime, difficulty, starReviews);
+    }
+    public String getCardDesigner(){
+        return this.cardDesigner = cardDesigner;
+    }
+    public void setCardDesigner(String cardDesigner){
+        this.cardDesigner = cardDesigner;
+    }
+}
