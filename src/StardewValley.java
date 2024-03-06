f2b1e81bca79ddfb9d1e8fa3013a9246ab70bbca
ejs6637 <160261312+ejs6637@users.noreply.github.com>
2024-03-06 17:09:17 -0500

    Update StardewValley.java
    edit

--- src/StardewValley.java
+++ src/StardewValley.java
@@ -1,2 +1,20 @@
-//public class StardewValley extends VideoGames{
-//}
+public class StardewValley extends VideoGames{
+    private String characterName;
+
+    public StardewValley(String gameName, String gameType, String creator,
+                     Double currentPrice, int maxPlayerNum, Double playingTime,
+                     String difficulty, Double starReviews) {
+
+        super(gameName, gameType, creator, currentPrice, maxPlayerNum,
+                playingTime, difficulty, starReviews);
+    }
+
+
+
+    public String getCharacterName(){
+        return this.characterName = characterName;
+    }
+    public void setCharacterName(String characterName){
+        this.characterName = characterName;
+    }
+}
