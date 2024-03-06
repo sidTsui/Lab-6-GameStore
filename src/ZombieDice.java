83d1a2b2be0e77eadc44b1717a4be02bde87bc6d
ejs6637 <160261312+ejs6637@users.noreply.github.com>
2024-03-06 17:14:50 -0500

    Update ZombieDice.java
    edit

--- src/ZombieDice.java
+++ src/ZombieDice.java
@@ -1,2 +1,18 @@
-//public class ZombieDice extends BoardGames{
-//}
+public class ZombieDice extends BoardGames{
+    private String specialItemsIncluded;
+
+    public ZombieDice(String gameName, String gameType, String creator,
+                     Double currentPrice, int maxPlayerNum, Double playingTime,
+                     String difficulty, Double starReviews) {
+
+        super(gameName, gameType, creator, currentPrice, maxPlayerNum,
+                playingTime, difficulty, starReviews);
+    }
+
+    public String getSpecialItemsIncluded(){
+        return this.specialItemsIncluded = specialItemsIncluded;
+    }
+    public void setSpecialItemsIncluded(String specialItemsIncluded){
+        this.specialItemsIncluded = specialItemsIncluded;
+    }
+}
