f47f742b2fb8325468f3a402a75dcdfa805daefe
ejs6637 <160261312+ejs6637@users.noreply.github.com>
2024-03-06 17:12:40 -0500

    Update WarHammer.java
    edit

--- src/WarHammer.java
+++ src/WarHammer.java
@@ -1,2 +1,18 @@
-//public class WarHammer extends BoardGames{
-//}
+public class WarHammer extends BoardGames{
+    private String specialItemsIncluded;
+
+    public WarHammer(String gameName, String gameType, String creator,
+                     Double currentPrice, int maxPlayerNum, Double playingTime,
+                     String difficulty, Double starReviews) {
+
+        super(gameName, gameType, creator, currentPrice, maxPlayerNum,
+                playingTime, difficulty, starReviews);
+    }
+
+    public String getSpecialItemsIncluded(){
+        return this.specialItemsIncluded = specialItemsIncluded;
+    }
+    public void setSpecialItemsIncluded(String specialItemsIncluded){
+        this.specialItemsIncluded = specialItemsIncluded;
+    }
+}
