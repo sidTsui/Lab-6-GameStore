83eb012c0b46469bbac4fb0574f57675d53e8e71
GeorgiaWatkins <160261340+GeorgiaWatkins@users.noreply.github.com>
2024-03-11 17:24:11 -0400

    Update LoveLetter.java

--- src/LoveLetter.java
+++ src/LoveLetter.java
@@ -1,6 +1,6 @@
 public class LoveLetter extends CardGames{
     private String cardDesigner;
-
+    // cardDesigner is what company / person designed the deck of cards in use
     public LoveLetter(String gameName, String gameType, String creator,
                      Double currentPrice, int maxPlayerNum, Double playingTime,
                      String difficulty, Double starReviews) {
