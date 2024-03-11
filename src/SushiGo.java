f944a72c715f403964924eaa3b0ab5abab846bf1
GeorgiaWatkins <160261340+GeorgiaWatkins@users.noreply.github.com>
2024-03-11 17:26:57 -0400

    Update SushiGo.java

--- src/SushiGo.java
+++ src/SushiGo.java
@@ -1,6 +1,7 @@
 public class SushiGo extends CardGames{
     private String cardDesigner;
-
+    // card designer is the name of the person / company who designed the deck of cards 
+    
     public SushiGo(String gameName, String gameType, String creator,
                      Double currentPrice, int maxPlayerNum, Double playingTime,
                      String difficulty, Double starReviews) {
