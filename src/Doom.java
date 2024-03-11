782414d26a282a940cbfbfcf3d53c2c633bd4736
GeorgiaWatkins <160261340+GeorgiaWatkins@users.noreply.github.com>
2024-03-11 17:21:52 -0400

    Update Doom.java

--- src/Doom.java
+++ src/Doom.java
@@ -1,5 +1,6 @@
 public class Doom extends VideoGames{
     private String characterName;
+    // character name is the name of the character the user selects to represent them 
 
     public Doom(String gameName, String gameType, String creator,
                      Double currentPrice, int maxPlayerNum, Double playingTime,
