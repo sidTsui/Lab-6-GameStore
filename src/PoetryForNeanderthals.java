a8ee50cd6a360a5fa1fee95456e06239f189d33e
GeorgiaWatkins <160261340+GeorgiaWatkins@users.noreply.github.com>
2024-03-11 17:24:58 -0400

    Update PoetryForNeanderthals.java

--- src/PoetryForNeanderthals.java
+++ src/PoetryForNeanderthals.java
@@ -1,5 +1,6 @@
 public class PoetryForNeanderthals extends BoardGames{
     private String specialItemsIncluded;
+    // special items include things like special dice, cards, or game pieces
 
     public PoetryForNeanderthals(String gameName, String gameType, String creator,
                      Double currentPrice, int maxPlayerNum, Double playingTime,
